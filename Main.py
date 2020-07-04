e7775fc3c83831928d62b6a33e3d53546730bc5e
mjip <marie.payne@hotmail.ca>
2020-07-04 13:52:54 -0400

    Finished the intro

--- Main.py
+++ Main.py
@@ -1,48 +1,30 @@
 from asciimatics.screen import Screen
-from asciimatics.scene import Scene
-from asciimatics.effects import Cycle, Stars
-from asciimatics.renderers import FigletText
 
 from scenes import intro
 
 def demo(screen):
-    screen.set_title("Quarantine Fourteen")
+	screen.set_title("Quarantine Fourteen")
+	intro.demo(screen)
 
-    scenes = []
-
-    # First scene- title sequence
-    effects = [
-        Cycle(
-            screen,
-            FigletText("WELCOME TO", font='big'),
-            screen.height // 2 - 8),
-        Cycle(
-            screen,
-            FigletText("QUARANTINE FOURTEEN", font='big'),
-            screen.height // 2 + 3),
-        Stars(screen, (screen.width + screen.height) // 2)
-    ]
-    screen.play([Scene(effects, 500)])
-
-Screen.wrapper(intro.demo)
+Screen.wrapper(demo)
 
 # ACTIONS
 # Format: (energy change, fulfillment change, repetitive effect)
 
 DRINK_BEER = (
-    -10,
-    +10,
-    # TODO: drunk_function?
+	-10,
+	+10,
+	# TODO: drunk_function?
 )
 MOVE_ROOM = (
-    -5,
-    0,
-    # TODO: decrease fulfillment multiplicatively
+	-5,
+	0,
+	# TODO: decrease fulfillment multiplicatively
 )
 
 # TIME OF DAY
 TIME_OF_DAY = {
-    "morning": ("dawn", "mid-morning"),
-    "afternoon": ("noon", "mid-afternoon"),
-    "night": ("6 pm", "8 pm", "10 pm", "midnight"),
+	"morning": ("dawn", "mid-morning"),
+	"afternoon": ("noon", "mid-afternoon"),
+	"night": ("6 pm", "8 pm", "10 pm", "midnight"),
 }
